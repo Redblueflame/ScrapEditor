ff8df6f88c5a24cb704fabb00effca268d49942e
Valentin Ricard <redblueflame1@gmail.com>
2019-08-03 11:42:44 +0200

    Added API calls for login

--- ScrapEditor/Startup.cs
+++ ScrapEditor/Startup.cs
@@ -40,8 +40,11 @@ namespace ScrapEditor
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            
             app.UseMvc();
         }
     }
